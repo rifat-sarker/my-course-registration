3e650ad303b6ca005308cbdbe4a07564c9441719
rifat-sarker <rifatswd@gmail.com>
2023-11-01 07:33:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-center'>Course Registration</h1>
+      
+     
+    </>
+  )
+}
+
+export default App
